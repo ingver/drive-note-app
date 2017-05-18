76099b84da2239729db9d745eb7309010a7c1385
Igor Schelkonogov <ingver.yum@gmail.com>
2017-05-18 13:30:56 +0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+
+<div id="app">
+  <p>Hello, {{ subject }}</p>
+</div>
+
+</template>
+
+
+<style scoped>
+
+p {
+  font-family: 'Roboto', sans-serif;
+  color: green;
+}
+
+</style>
+
+
+<script>
+
+export default {
+  data() {
+    return {
+      subject: 'Drive'
+    }
+  }
+}
+
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const Webpack = require('webpack')
+
+const config = {
+  entry: './src/main.js',
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/dist/',
+    filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        exclude: /node_modules/,
+        use: 'vue-loader'
+      }
+    ],
+  },
+
+  devtool: 'cheap-eval-sourcemaps',
+
+  plugins: [
+    new Webpack.HotModuleReplacementPlugin(),
+    new Webpack.SourceMapDevToolPlugin({
+      filename: '[name].map',
+    })
+  ],
+
+  devServer: {
+    contentBase: __dirname,
+    port: 9000,
+    hot: true,
+    noInfo: true,
+    overlay: true,
+  }
+}
+
+module.exports = config
